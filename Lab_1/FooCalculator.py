eeb87b02ccf9d190dcd265a6e42f29bb09d35f68
mariacristina <criucc@tiscali.it>
2018-10-02 17:27:45 +0200

    second commit for the corrections of the code

--- Lab_1/FooCalculator.py
+++ Lab_1/FooCalculator.py
@@ -5,3 +5,9 @@ class FooCalculator:
     def __init__(self):
         pass
 
+
+    def sum(self,m,n):
+    	return c.sum(m,n)
+
+    def divide(self,m,n):
+    	return c.divide(m,n)
--- Lab_1/calculator.py
+++ Lab_1/calculator.py
@@ -2,11 +2,11 @@ m = 2;
 n = 5;
 
 
-def somma(m, n):
+def sum(m, n):
     result = m
     if n<0:
         for i in range(abs(n)):
-            result += 1
+            result -= 1
         return result
     else:
         for i in range(n):
@@ -15,11 +15,15 @@ def somma(m, n):
 
 
 def divide(m,n):
+
+    if n==0:
+        raise ZeroDivisionError('You cannot divide by 0!')
+        
     result = 0;
     negativeResult = m>0 and n<0 or m<0 and n>0
     n= abs(n)
     m= abs(m)
-
+   
     while(m-n >= 0):
         m -= n
         result+=1
